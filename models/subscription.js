08d9ea94785f3eeb00ba64879a616187632f1b76
MathewKingwill <matkingwill@gmail.com>
2018-06-15 11:56:55 +0200

    second commit

--- models/subscription.js
+++ models/subscription.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
-
-
+var express = require('express');
+var router = express.Router();
 //create schema for a quote
-var subscription = mongoose.Schema({
-    email: String,
+var subscriptionSchema = mongoose.Schema({
+    email: String
 
 });
 
 
 
 //create a model from the schema
-var subscription = mongoose.model('subscription', subscription);
+var Subscription = mongoose.model('Subscription', subscriptionSchema);
 
 
-module.exports = subscription;
+module.exports = Subscription;
--- server.js
+++ server.js
@@ -7,57 +7,26 @@ var ejs = require('ejs')
 var mongoose = require('mongoose');
 var router = express.Router();
 var subscription = require('./models/subscription');
-mongoose.connect('mongodb://matkingwill:cuan1234@ds237610.mlab.com:37610/newsletter')
+
+mongoose.connect('mongodb://matkingwill:cuan1234@ds237610.mlab.com:37610/newsletter');
 
 var app = express();
 
 
 
-// router.post('/subscriber', function(req, res, next) {
-//
-//
-//
-//   //get the fields sent to us
-//   //create a new model
-//   //save in the database
-//   var subscription = new subcription({
-//     email: req.body.email,
-//
-//    }
-//   );
-//
-//
-//   new_subscription.save(function (err) {
-//     if (err) return handleError(err);
-//
-//
-//       res.redirect('/')
-//   });
-//
-//
-//
-//
-//   //// res.json({ message: 'LETS SAVE THIS QUOTE NOW' })
-//
-//
-//   //  res.render('index', { title: 'Express',quote : "A bird in the hand is worth 2 in the bush." });
-//
-//   });
-//
-//
-//
-// module.exports = router;
-router.post('/subscription', function(req, res, next){
 
- var subscription = new Subscribe();
- 
- subscriber.email = req.body.email;
+router.post('/', function(req, res, next){
+
+ var subscription = new subscription();
+
+ subscription.email = req.body.email;
 
  subscription.save(function (err) {
-     if (error)
-         res.send(err);
- });
- res.redirect('/');
+     if (error) return handleError(err);
+
+
+ res.redirect('/')
+});
 });
 app.use( express.static( "public" ) );
 
